f1b2009b5bd905d3c25388f0cbc0bf0783211b35
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-09-24 21:40:08 -0400

    Add footer with menu inserts

--- functions.php
+++ functions.php
@@ -12,7 +12,9 @@ function register_theme_menus() {
 	register_nav_menus(
 		array(
 			'primary-menu' => __( 'Primary Menu' ),
-			'footer-social-media-menu' => __( 'Social Media' )
+			'footer-menu-bottom' => __( 'Footer Menu - Bottom' ),
+			'footer-column-2' => __( 'Footer Menu - Column 2' ),
+			'footer-column-3' => __( 'Footer Menu - Column 3' )
 		)
 	);
 
--- partials/footer/footer-bottom.php
+++ partials/footer/footer-bottom.php
@@ -4,59 +4,25 @@
 
 	<div class="container clearfix">
 
-		<div class="col_half">
-			Copyrights &copy; <?php echo date("Y"); ?> All Rights Reserved by Blackford Initiative.<br>
-			<div class="copyright-links"><a href="/terms-of-use">Terms of Use</a> / <a href="/privacy-policy">Privacy Policy</a></div>
-		</div>
+		<div class="col_full nobottommargin center">
+			<div class="copyrights-menu copyright-links clearfix">
+			<?php
 
-		<div class="col_half col_last tright">
-			<!--<div class="fright clearfix">
-				<a href="#" class="social-icon si-small si-borderless si-facebook">
-					<i class="icon-facebook"></i>
-					<i class="icon-facebook"></i>
-				</a>
+				$footerDefaults = array(
+					'container' => false,
+					'theme_location' => 'footer-menu-bottom',
+					'items_wrap' => '',
+					'before' => '',
+					'after' => ' / '
+				);
+				wp_nav_menu( $footerDefaults );
 
-				<a href="#" class="social-icon si-small si-borderless si-twitter">
-					<i class="icon-twitter"></i>
-					<i class="icon-twitter"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-gplus">
-					<i class="icon-gplus"></i>
-					<i class="icon-gplus"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-pinterest">
-					<i class="icon-pinterest"></i>
-					<i class="icon-pinterest"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-vimeo">
-					<i class="icon-vimeo"></i>
-					<i class="icon-vimeo"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-github">
-					<i class="icon-github"></i>
-					<i class="icon-github"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-yahoo">
-					<i class="icon-yahoo"></i>
-					<i class="icon-yahoo"></i>
-				</a>
-
-				<a href="#" class="social-icon si-small si-borderless si-linkedin">
-					<i class="icon-linkedin"></i>
-					<i class="icon-linkedin"></i>
-				</a>
+			?>
 			</div>
-
-			<div class="clear"></div> -->
-
-			<i class="icon-envelope2"></i> positiveblackfordnews@gmail.com
+			Copyrights &copy; 2019 All Rights Reserved by Blackford Initiative.<br>
+			Developed by: <a href="http://dominiccartwright.com" target="_blank">Dominic Cartwright</a> &amp; <a href="http://kwmcgrego.com" target="_blank">Kevin McGregor</a>
 		</div>
 
 	</div>
 
-</div><!-- #copyrights end -->
+</div>
