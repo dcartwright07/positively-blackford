3706ea16e79392a85c8a4c0ccbe343b5dbd5d635
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-09-25 19:53:29 -0400

    Update first column of top footer

--- partials/footer/footer-top.php
+++ partials/footer/footer-top.php
@@ -9,21 +9,11 @@
 			<!-- <img src="images/footer-widget-logo.png" alt="" class="footer-logo"> -->
 
 			<p>We look to share <strong>Positive</strong> news about Blackford County.</p>
-
-			<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
-				<address>
-					<strong>Headquarters:</strong><br>
-					000 Street<br>
-					Hartford City, IN 00000<br>
-				</address>
-				<!-- <abbr title="Phone Number"><strong>Phone:</strong></abbr> (91) 8547 632521<br> -->
-				<!-- <abbr title="Fax"><strong>Fax:</strong></abbr> (91) 11 4752 1433<br> -->
-				<abbr title="Email Address"><strong>Email:</strong></abbr> positiveblackfordnews@gmail.com
-			</div>
+			<p><i class="icon-line-mail"></i>&nbsp;&nbsp;positiveblackfordnews@gmail.com</p>
 
 			<div class="row">
 				<div class="col-lg-6 clearfix bottommargin-sm">
-					<a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+					<a href="https://www.facebook.com/positivelyblackford/" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
 						<i class="icon-facebook"></i>
 						<i class="icon-facebook"></i>
 					</a>
