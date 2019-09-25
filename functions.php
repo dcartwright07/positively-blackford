d5f3b3689cfaa7c0c78cd228647a64183e746e75
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-09-25 19:22:22 -0400

    Clean functions file

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@
 add_theme_support( 'menus' );
 add_theme_support( 'post-thumbnails' );
 
-/* #region Initialize menus to use in the admin panel */
+/* #region Initializations */
 
+// Menus to use in the admin panel
 function register_theme_menus() {
 
 	register_nav_menus(
@@ -20,6 +21,17 @@ function register_theme_menus() {
 
 }
 add_action( 'init', 'register_theme_menus' );
+
+// Add SVG image support to available MIME types
+function add_file_types_to_uploads( $file_types ) {
+
+	$new_filetypes = array();
+	$new_filetypes[ 'svg' ] = 'image/svg+xml';
+	$file_types = array_merge( $file_types, $new_filetypes );
+	return $file_types;
+
+}
+add_filter( 'upload_mimes', 'add_file_types_to_uploads' );
 
 /* #endregion */
 
@@ -50,18 +62,6 @@ function pb_theme_js() {
 add_action( 'wp_enqueue_scripts', 'pb_theme_js' );
 
 /* #endregion */
-
-// Add SVG image support to available MIME types
-// ====================================
-function add_file_types_to_uploads( $file_types ) {
-
-	$new_filetypes = array();
-	$new_filetypes[ 'svg' ] = 'image/svg+xml';
-	$file_types = array_merge( $file_types, $new_filetypes );
-	return $file_types;
-
-}
-add_filter( 'upload_mimes', 'add_file_types_to_uploads' );
 
 /* #region Home page article listings */
 
@@ -161,7 +161,7 @@ function pb_theme_comment( $comment, $args, $depth ) {
 }
 /* #endregion */
 
-/* #regin Custom Horizontal Navigation for the Bottom Footer Section */
+/* #region Custom Horizontal Navigation for the Bottom Footer Section */
 
 function footer_bottom_nav() {
 
