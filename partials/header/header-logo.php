2020571bad144c0b227329137cfb897124fa6f1f
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-09-26 21:11:42 -0400

    Add color to logo

--- partials/header/header-logo.php
+++ partials/header/header-logo.php
@@ -1,10 +1,10 @@
 <!-- Logo
 ============================================= -->
 <div id="logo">
-	<a href="/" class="standard-logo" data-dark-logo="images/logo.png">
-		<?php echo '<img src="' . get_template_directory_uri() . '/assets/images/logo.png" alt="Positively Blackford Logo">'; ?>
+	<a href="/" class="standard-logo" data-dark-logo="images/logo-blue.png">
+		<?php echo '<img src="' . get_template_directory_uri() . '/assets/images/logo-blue.png" alt="Positively Blackford Logo">'; ?>
 	</a>
-	<a href="/" class="retina-logo" data-dark-logo="images/logo2x.png">
-		<?php echo '<img src="' . get_template_directory_uri() . '/assets/images/logo2x.png" alt="Positively Blackford Logo">'; ?>
+	<a href="/" class="retina-logo" data-dark-logo="images/logo2x-blue.png">
+		<?php echo '<img src="' . get_template_directory_uri() . '/assets/images/logo2x-blue.png" alt="Positively Blackford Logo">'; ?>
 	</a>
 </div><!-- #logo end -->
