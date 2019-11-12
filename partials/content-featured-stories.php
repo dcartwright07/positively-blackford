0f522dbecadd10ceddbc5b071805b8875cc3a10b
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-11-11 20:55:20 -0500

    Remove unnecessary space

--- partials/content-featured-stories.php
+++ partials/content-featured-stories.php
@@ -2,24 +2,21 @@
 	<div class="fslider flex-thumb-grid grid-6" data-speed="500" data-arrows="true" data-thumbs="false" data-easing="easeOutQuad">
 		<div class="flexslider">
 			<div class="slider-wrap">
-
 				<?php
 
-					$num_posts = -1;
-					if( is_front_page() ) $num_posts = 6;
+				$num_posts = -1;
+				if( is_front_page() ) $num_posts = 6;
 
-					$args = array(
-						'posts_per_page' => $num_posts
-					);
-					$query =  new WP_Query( $args );
+				$args = array(
+					'posts_per_page' => $num_posts
+				);
+				$query =  new WP_Query( $args );
 
-					if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
-
-						$featured = get_field( 'featured_story' );
-						if( $featured == 'yes' ) :
+				if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
+					$featured = get_field( 'featured_story' );
+					if( $featured == 'yes' ) :
 
 				?>
-
 				<div class="slide" data-thumb="<?php the_post_thumbnail_url( 'thumbnail' ); ?>">
 					<a href="<?php the_permalink(); ?>">
 						<?php the_post_thumbnail( 'post-thumbnail' ); ?>
@@ -32,13 +29,13 @@
 									<span>
 										<?php
 
-											$category = get_the_category( $post->ID );
-											foreach( $category as $cat ) {
-												echo $cat->cat_name;
-												if( next( $category ) ) {
-													echo ', ';
-												}
+										$category = get_the_category( $post->ID );
+										foreach( $category as $cat ) {
+											echo $cat->cat_name;
+											if( next( $category ) ) {
+												echo ', ';
 											}
+										}
 
 										?>
 									</span>
@@ -47,25 +44,20 @@
 						</div>
 					</a>
 				</div>
-
 				<?php endif; endwhile; endif; ?>
-
 			</div>
 		</div>
 	</div>
 </div>
 
 <div class="col_two_fifth bottommargin-lg col_last">
-
 	<?php
 
-		if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
-
-			$featured = get_field( 'featured_story' );
-			if( $featured == 'yes' ) :
+	if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post();
+		$featured = get_field( 'featured_story' );
+		if( $featured == 'yes' ) :
 
 	?>
-
 	<div class="spost clearfix">
 		<div class="entry-c">
 			<div class="entry-title">
@@ -73,9 +65,7 @@
 			</div>
 		</div>
 	</div>
-
 	<?php endif; endwhile; endif; ?>
-
 </div>
 
 <div class="clear"></div>
