72755459b4a515820680764598164c1de45e2971
Dominic Cartwright <dominic.cartwright07@gmail.com>
2020-11-28 16:59:12 -0500

    Update local dev environment

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,24 @@
-//#region  Switch mobile top menu
+// #region  Switch mobile top menu
 
-var element = jQuery('.top-right-links');
+var element = jQuery(".top-right-links");
 var windowWidth = jQuery(window).width();
 
-if(windowWidth <= 992) {
-	element.removeClass('fright');
+if (windowWidth <= 992) {
+	element.removeClass("fright");
 }
 
-//#endregion
-//#region Defer JavaScript from Youtube Videos
+// #endregion
+// #region Defer JavaScript from Youtube Videos
 
 function pbDeferYoutubeJS() {
+	var vidDefer = document.getElementsByTagName("iframe");
 
-	var vidDefer = document.getElementsByTagName('iframe');
-
-	for(var i = 0; i < vidDefer.length; i++) {
-		if(vidDefer[i].getAttribute('data-src')) {
-			vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+	for (var i = 0; i < vidDefer.length; i++) {
+		if (vidDefer[i].getAttribute("data-src")) {
+			vidDefer[i].setAttribute("src", vidDefer[i].getAttribute("data-src"));
 		}
 	}
-
 }
 window.onload = pbDeferYoutubeJS;
 
-//#endregion
+// #endregion
